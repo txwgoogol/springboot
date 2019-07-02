b50151f5caabc34125952f8ccffa64b7b52d0954
txwgoogol <txwgoogol@gmail.com>
2019-07-02 13:41:01 +0800

    add getter/setter with entity

--- src/main/java/com/example/demo/java/domain/User.java
+++ src/main/java/com/example/demo/java/domain/User.java
@@ -1,76 +1,27 @@
 package com.example.demo.java.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
+
+import javax.persistence.*;
 
 /**
  * 用户实体类
  *
  * @author colin
  */
+@Data
 @Entity
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)//字增长策略
+    @Column(name = "id")
     private Long id;//唯一标识
+    @Column(name = "name")
     private String name;
+    @Column(name = "age")
     private Integer age;
+    @Column(name = "email")
     private String email;
-
-    //无参默认构造器
-    public User() {
-    }
-
-    public User(Long id, String name, Integer age, String email) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", email='" + email + '\'' +
-                '}';
-    }
 
 }
